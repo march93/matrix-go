245ee3c6b375012b497cfb58ad6770026b3af373
Marc <marc.ho380@gmail.com>
2023-02-02 00:02:52 -0800

    Add files

---
+++ main.go
@@ -0,0 +1,184 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+/*
+ *	Start app with `go run .`
+ *	For files, you can use 'file=@/path/matrix.csv', 'file=@/path/matrix-invalid.csv', or 'file=@/path/matrix-empty.csv'
+ *	To run tests, use `go test`
+ */
+
+func main() {
+	// Let handler handle the routes
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// Divert to the appropriate method based on URL path
+	if r.URL.Path == "/echo" {
+		echo(w, r)
+	} else if r.URL.Path == "/invert" {
+		invert(w, r)
+	} else if r.URL.Path == "/flatten" {
+		flatten(w, r)
+	} else if r.URL.Path == "/sum" {
+		sum(w, r)
+	} else if r.URL.Path == "/multiply" {
+		multiply(w, r)
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write(([]byte(fmt.Sprintln("error endpoint not found"))))
+	}
+}
+
+// Extract method for reading file
+func readFile(w http.ResponseWriter, r *http.Request) [][]string {
+	file, _, err := r.FormFile("file")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(fmt.Sprintf("error %s", err.Error())))
+		return nil
+	}
+	defer file.Close()
+	records, err := csv.NewReader(file).ReadAll()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(fmt.Sprintf("error %s", err.Error())))
+		return nil
+	}
+	validateData(records, w)
+	return records
+}
+
+/*
+ *	curl -F 'file=@/path/matrix.csv' "localhost:8080/echo"
+ */
+func echo(w http.ResponseWriter, r *http.Request) {
+	records := readFile(w, r)
+	var response string
+
+	for _, row := range records {
+		response = fmt.Sprintf("%s%s\n", response, strings.Join(row, ","))
+	}
+	fmt.Fprint(w, response)
+}
+
+/*
+ *	curl -F 'file=@/path/matrix.csv' "localhost:8080/invert"
+ */
+func invert(w http.ResponseWriter, r *http.Request) {
+	records := readFile(w, r)
+	var response string
+
+	// Create new slices to populate our inverted matrix
+	inverted := make([][]string, len(records))
+	for i := range inverted {
+		// Initialize each row
+		inverted[i] = make([]string, len(records))
+	}
+
+	for i, row := range records {
+		for j := range row {
+			// Invert by taking the opposing positions
+			inverted[i][j] = records[j][i]
+			inverted[j][i] = records[i][j]
+		}
+	}
+
+	for _, row := range inverted {
+		response = fmt.Sprintf("%s%s\n", response, strings.Join(row, ","))
+	}
+	fmt.Fprint(w, response)
+}
+
+/*
+ *	curl -F 'file=@/path/matrix.csv' "localhost:8080/flatten"
+ */
+func flatten(w http.ResponseWriter, r *http.Request) {
+	records := readFile(w, r)
+	slice := returnAllNums(records)
+
+	fmt.Fprint(w, strings.Join(slice, ","), "\n")
+}
+
+/*
+ *	curl -F 'file=@/path/matrix.csv' "localhost:8080/sum"
+ */
+func sum(w http.ResponseWriter, r *http.Request) {
+	records := readFile(w, r)
+	slice := returnAllNums(records)
+
+	total := 0
+	for _, num := range slice {
+		// Convert string to integer first
+		current, err := strconv.Atoi(num)
+
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(fmt.Sprintf("error %s", err.Error())))
+		}
+		total += current
+	}
+	fmt.Fprint(w, total, "\n")
+}
+
+/*
+ *	curl -F 'file=@/path/matrix.csv' "localhost:8080/multiply"
+ */
+func multiply(w http.ResponseWriter, r *http.Request) {
+	records := readFile(w, r)
+	slice := returnAllNums(records)
+
+	total := 1
+	for _, num := range slice {
+		// Convert string to integer first
+		current, err := strconv.Atoi(num)
+
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(fmt.Sprintf("error %s", err.Error())))
+		}
+		total *= current
+	}
+	fmt.Fprint(w, total, "\n")
+}
+
+func validateData(records [][]string, w http.ResponseWriter) {
+	rows := len(records)
+
+	if rows == 0 {
+		// Empty csv provided
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(fmt.Sprintln("error no data provided")))
+	}
+
+	for _, row := range records {
+		if len(row) != rows {
+			// Row and column lengths should be equal
+			w.WriteHeader(http.StatusBadGateway)
+			w.Write([]byte(fmt.Sprintln("error unequal rows and column lengths")))
+		}
+	}
+}
+
+func returnAllNums(records [][]string) []string {
+	// Create a single slice with length equal to the total number of elements
+	// This will make it easier to go through each element one by one
+	slice := make([]string, len(records)*len(records))
+	i := 0
+
+	for _, row := range records {
+		for _, num := range row {
+			slice[i] = num
+			i++
+		}
+	}
+	return slice
+}
---
+++ main_test.go
@@ -0,0 +1,259 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+func TestEcho(t *testing.T) {
+	fileName := "matrix.csv"
+	filePath := path.Join("./", fileName)
+
+	file, _ := os.Open(filePath)
+	defer file.Close()
+
+	// Create the form data to pass in our request
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, _ := writer.CreateFormFile("file", filepath.Base(file.Name()))
+	io.Copy(part, file)
+	writer.Close()
+
+	ts := httptest.NewServer(http.HandlerFunc(handler))
+	defer ts.Close()
+
+	// Serve up our request
+	res, err := http.Post(ts.URL+"/echo", writer.FormDataContentType(), body)
+	if err != nil {
+		t.Fatalf("error making POST request to echo: %s", err)
+	}
+
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("expected HTTP status of %d, got %d", http.StatusOK, res.StatusCode)
+	}
+
+	builder := new(strings.Builder)
+	io.Copy(builder, res.Body)
+
+	if strings.Compare(strings.Trim(builder.String(), "\r\n"), "1,2,3\n4,5,6\n7,8,9") != 0 {
+		t.Errorf("expected content of \n%s\ngot\n%s\n", "1,2,3\n4,5,6\n7,8,9", builder.String())
+	}
+}
+
+func TestInvert(t *testing.T) {
+	fileName := "matrix.csv"
+	filePath := path.Join("./", fileName)
+
+	file, _ := os.Open(filePath)
+	defer file.Close()
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, _ := writer.CreateFormFile("file", filepath.Base(file.Name()))
+	io.Copy(part, file)
+	writer.Close()
+
+	ts := httptest.NewServer(http.HandlerFunc(handler))
+	defer ts.Close()
+
+	res, err := http.Post(ts.URL+"/invert", writer.FormDataContentType(), body)
+	if err != nil {
+		t.Fatalf("error making POST request to invert: %s", err)
+	}
+
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("expected HTTP status of %d, got %d", http.StatusOK, res.StatusCode)
+	}
+
+	builder := new(strings.Builder)
+	io.Copy(builder, res.Body)
+
+	if strings.Compare(strings.Trim(builder.String(), "\r\n"), "1,4,7\n2,5,8\n3,6,9") != 0 {
+		t.Errorf("expected content of %s got %s", "1,4,7\n2,5,8\n3,6,9", builder.String())
+	}
+}
+
+func TestFlatten(t *testing.T) {
+	fileName := "matrix.csv"
+	filePath := path.Join("./", fileName)
+
+	file, _ := os.Open(filePath)
+	defer file.Close()
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, _ := writer.CreateFormFile("file", filepath.Base(file.Name()))
+	io.Copy(part, file)
+	writer.Close()
+
+	ts := httptest.NewServer(http.HandlerFunc(handler))
+	defer ts.Close()
+
+	res, err := http.Post(ts.URL+"/flatten", writer.FormDataContentType(), body)
+	if err != nil {
+		t.Fatalf("error making POST request to flatten: %s", err)
+	}
+
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("expected HTTP status of %d, got %d", http.StatusOK, res.StatusCode)
+	}
+
+	builder := new(strings.Builder)
+	io.Copy(builder, res.Body)
+
+	if strings.Compare(strings.Trim(builder.String(), "\r\n"), "1,2,3,4,5,6,7,8,9") != 0 {
+		t.Errorf("expected content of %s got %s", "1,2,3,4,5,6,7,8,9", builder.String())
+	}
+}
+
+func TestSum(t *testing.T) {
+	fileName := "matrix.csv"
+	filePath := path.Join("./", fileName)
+
+	file, _ := os.Open(filePath)
+	defer file.Close()
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, _ := writer.CreateFormFile("file", filepath.Base(file.Name()))
+	io.Copy(part, file)
+	writer.Close()
+
+	ts := httptest.NewServer(http.HandlerFunc(handler))
+	defer ts.Close()
+
+	res, err := http.Post(ts.URL+"/sum", writer.FormDataContentType(), body)
+	if err != nil {
+		t.Fatalf("error making POST request to sum: %s", err)
+	}
+
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("expected HTTP status of %d, got %d", http.StatusOK, res.StatusCode)
+	}
+
+	builder := new(strings.Builder)
+	io.Copy(builder, res.Body)
+
+	if strings.Compare(strings.Trim(builder.String(), "\r\n"), "45") != 0 {
+		t.Errorf("expected content of %s got %s", "45", builder.String())
+	}
+}
+
+func TestMultiply(t *testing.T) {
+	fileName := "matrix.csv"
+	filePath := path.Join("./", fileName)
+
+	file, _ := os.Open(filePath)
+	defer file.Close()
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, _ := writer.CreateFormFile("file", filepath.Base(file.Name()))
+	io.Copy(part, file)
+	writer.Close()
+
+	ts := httptest.NewServer(http.HandlerFunc(handler))
+	defer ts.Close()
+
+	res, err := http.Post(ts.URL+"/multiply", writer.FormDataContentType(), body)
+	if err != nil {
+		t.Fatalf("error making POST request to multiply: %s", err)
+	}
+
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("expected HTTP status of %d, got %d", http.StatusOK, res.StatusCode)
+	}
+
+	builder := new(strings.Builder)
+	io.Copy(builder, res.Body)
+
+	if strings.Compare(strings.Trim(builder.String(), "\r\n"), "362880") != 0 {
+		t.Errorf("expected content of %s got %s", "362880", builder.String())
+	}
+}
+
+func TestInvalidData(t *testing.T) {
+	fileName := "matrix-invalid.csv"
+	filePath := path.Join("./", fileName)
+
+	file, _ := os.Open(filePath)
+	defer file.Close()
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, _ := writer.CreateFormFile("file", filepath.Base(file.Name()))
+	io.Copy(part, file)
+	writer.Close()
+
+	ts := httptest.NewServer(http.HandlerFunc(handler))
+	defer ts.Close()
+
+	res, err := http.Post(ts.URL+"/echo", writer.FormDataContentType(), body)
+	if err != nil {
+		t.Fatalf("error making POST request to echo: %s", err)
+	}
+
+	if res.StatusCode != http.StatusInternalServerError {
+		t.Errorf("expected HTTP status of %d, got %d", http.StatusInternalServerError, res.StatusCode)
+	}
+}
+
+func TestEmpty(t *testing.T) {
+	fileName := "matrix-empty.csv"
+	filePath := path.Join("./", fileName)
+
+	file, _ := os.Open(filePath)
+	defer file.Close()
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, _ := writer.CreateFormFile("file", filepath.Base(file.Name()))
+	io.Copy(part, file)
+	writer.Close()
+
+	ts := httptest.NewServer(http.HandlerFunc(handler))
+	defer ts.Close()
+
+	res, err := http.Post(ts.URL+"/echo", writer.FormDataContentType(), body)
+	if err != nil {
+		t.Fatalf("error making POST request to echo: %s", err)
+	}
+
+	if res.StatusCode != http.StatusBadRequest {
+		t.Errorf("expected HTTP status of %d, got %d", http.StatusBadRequest, res.StatusCode)
+	}
+}
+
+func TestBadUrl(t *testing.T) {
+	fileName := "matrix.csv"
+	filePath := path.Join("./", fileName)
+
+	file, _ := os.Open(filePath)
+	defer file.Close()
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, _ := writer.CreateFormFile("file", filepath.Base(file.Name()))
+	io.Copy(part, file)
+	writer.Close()
+
+	ts := httptest.NewServer(http.HandlerFunc(handler))
+	defer ts.Close()
+
+	res, err := http.Post(ts.URL+"/bad", writer.FormDataContentType(), body)
+	if err != nil {
+		t.Fatalf("error making POST request to bad: %s", err)
+	}
+
+	if res.StatusCode != http.StatusNotFound {
+		t.Errorf("expected HTTP status of %d, got %d", http.StatusNotFound, res.StatusCode)
+	}
+}
